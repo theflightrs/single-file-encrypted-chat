b94fe3e4295f078b625612b111a5ec48fce1484f
getl0st <vladik.ko@hotmail.com>
2019-10-17 06:59:27 +0100

    Uodated

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'chat');
 define('DB_USER', 'root');
-define('DB_PASS', '2fFucyc7Nw');
+define('DB_PASS', '');
 
 if (isset($_GET['logout'])) {
     
